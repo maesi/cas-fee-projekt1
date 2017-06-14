1252895a8b7dcade48beaacb11f2d1045a605e80
Arnold Marcel (U116399) <marcel.arnold@mobi.ch>
2017-06-14 17:30:59 +0200

    Liste der Notizen anzeigen

--- client/js/application.js
+++ client/js/application.js
@@ -38,10 +38,68 @@
     namespace.loader = loader.getInstance();
 })(window.templates = window.templates || {});
 
+;(function (namespace) {
+    'use strict';
+
+    let model = (function () {
+        let instance;
+
+        class Model {
+            constructor() {
+                this.model = {
+                    headerConfig: {
+                        createAvailable: true
+                    },
+                    notes: [
+                        {
+                            titel: "Note Pro App",
+                            beschreibung: "Das Projekt 1 fürs CAS FEE erstellen",
+                            wichtigkeit: 5,
+                            erstellt: "2017-06-01",
+                            faelligkeit: "2017-07-02",
+                            erledigt: null
+                        }, {
+                            titel: "Sponsoren CRM",
+                            beschreibung: "CRM für Raphi",
+                            wichtigkeit: 4,
+                            erstellt: "2017-06-01",
+                            faelligkeit: "2017-06-20",
+                            erledigt: null
+                        }
+                    ]
+                };
+            }
+
+            getNotes() {
+                return this.model.notes;
+            }
+
+            getHeaderConfig() {
+                return this.model.headerConfig;
+            }
+        }
+
+        return {
+            getInstance: function () {
+                if (!instance) {
+                    instance = new Model()
+                }
+                return instance;
+            }
+        };
+    }());
+
+    namespace.model = model.getInstance();
+})(window.model = window.model || {});
+
 
 $(window).ready(() => {
-    window.templates.loader.get("header", { createAvailable: true })
+    window.templates.loader.get("header", window.model.model.getHeaderConfig())
         .then((template) => {
             $('header').html(template);
+        });
+    window.templates.loader.get("edit", window.model.model.getNotes())
+        .then((template) => {
+            $('main').html(template);
         });
 });
