adf74ccd488a3d7eb3d7a6c0ebdaa06028558396
Arnold Marcel (U116399) <marcel.arnold@mobi.ch>
2017-06-29 16:17:56 +0200

    create REST Client

---
+++ client/js/rest.js
@@ -0,0 +1,41 @@
+;(function (service,$) {
+
+    function execute({url='/notes', method='GET', data}) {
+        let request = {
+            dataType: 'json',
+            contentType: 'application/json',
+            method: method,
+            url: "/rest" + url
+        };
+        if(data) {
+            request.data = JSON.stringify(data);
+        }
+        return $.ajax(request);
+    }
+
+    function getAll(){
+        return execute({});
+    }
+
+    function getById(id) {
+        return execute({url:'/notes/' + id});
+    }
+
+    function create(note) {
+        return execute({method: 'POST', data: note});
+    }
+
+    function update(note) {
+        return execute({url:'/notes/' + note._id, method: 'PUT', data: note});
+    }
+
+    service.rest = {
+        getAll : getAll,
+        getById: getById,
+        create: create,
+        update: update
+    };
+
+}(window.service = window.service || { }, jQuery));
+
+
